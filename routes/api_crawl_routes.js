a4f28040eab17b18d8190ce2203b8bab27c739be
jasonkim7288 <jason.kim7288@gmail.com>
2020-10-19 15:01:35 +1000

    added index controller

--- routes/api_crawl_routes.js
+++ routes/api_crawl_routes.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const CrawlsController = require('../controllers/api_crawl_controller');
+
+router.get('/start', CrawlsController.start);
+router.get('/check', CrawlsController.check);
+router.get('/create', CrawlsController.create);
+router.get('/addCrawl', CrawlsController.addCrawl);
+
 module.exports = router;
