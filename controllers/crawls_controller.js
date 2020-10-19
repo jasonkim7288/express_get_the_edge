0101bfc2b87fc621a8faa40e32cf2fe44cba7d13
Thomas Doig <thomasdoig@hotmail.com>
2020-10-19 14:59:15 +1000

    added to crawls controller and crawls utils

--- controllers/crawls_controller.js
+++ controllers/crawls_controller.js
@@ -1,19 +1,26 @@
-const {addCrawl} = require('../utils/crawls_utils');
+const {
+  addCrawl,
+  removeCrawl,
+  updateCrawl,
+  getAllCrawls
+} = require('../utils/crawls_utils');
 
 
 
 module.exports = {
-  index: (req, res) => {
-
+  index: async (req, res) => {
+    const allCrawls = await getAllCrawls(req);
+    console.log('allCrawls: ', allCrawls);
+    res.send(allCrawls);
   },
   create: (req, res) => {
     console.log(req.body);
     addCrawl(req).save((err, crawl) => {
       if (err) {
-          res.status(500);
-          return res.json({
-              error: err.message
-          });
+        res.status(500);
+        return res.json({
+          error: err.message
+        });
       }
       res.status(201);
       res.send(crawl);
@@ -29,9 +36,38 @@ module.exports = {
 
   },
   update: (req, res) => {
-
+    if (req.error) {
+      res.status(req.error.status);
+      res.send(req.error.message);
+  } else {
+      // execute the query from updatePost
+      updateCrawl(req).exec((err, crawl) => {
+          if (err) {
+              res.status(500);
+              return res.json({
+                  error: err.message
+              });
+          }
+          res.status(200);
+          res.send(crawl);
+      });
+  }
   },
   destroy: (req, res) => {
-
+    if (req.error) {
+      res.status(req.error.status);
+      res.send(req.error.message);
+    } else {
+      // execute the query from deletePost
+      removeCrawl(req.params.id).exec((err) => {
+        if (err) {
+          res.status(500);
+          return res.json({
+            error: err.message
+          });
+        }
+        res.sendStatus(204);
+      });
+    }
   }
 }
--- utils/crawls_utils.js
+++ utils/crawls_utils.js
@@ -1,13 +1,40 @@
 const User = require('../models/user');
 const Crawl = require('../models/crawl');
+const crawls_controller = require('../controllers/crawls_controller');
+const { find } = require('../models/user');
+const crawl = require('../models/crawl');
 
-const addCrawl = function (req) {
+
+const addCrawl = (req) => {
   return new Crawl(req.body);
 };
 
+const getAllCrawls = async (req) => {
+    let foundCrawls = await Crawl.find({isDefault: true});
+    console.log('foundCrawls:', foundCrawls);
+    if (req.user) {
+        req.user.crawls.forEach(async crawl => {
+            const foundUserCrawl = await Crawl.findById(crawl.crawl);
+            foundCrawls.push(foundUserCrawl);
+        });
+    }
+    return foundCrawls;
+};
 
+const removeCrawl = (id) => {
+    return Crawl.findByIdAndRemove(id);
+};
+
+const updateCrawl = (req) => {
+    return Crawl.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+    });
+};
 
 
 module.exports = {
-    addCrawl
+    addCrawl,
+    removeCrawl,
+    updateCrawl,
+    getAllCrawls
 };
